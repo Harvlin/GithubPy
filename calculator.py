ad033fc248e49cfd84ee13e4bbbd82dd0a0bf970
Harvlin <harvlinmaximillian@gmail.com>
2023-07-01 01:32:55 +0700

    Demo commit

--- calculator.py
+++ calculator.py
@@ -1,36 +1,26 @@
-# importing...
-import math
-# defining function
-def mathematics_function():
-    # make a loop
-    while True:
-        # taking user input information
-        print("What operation do you want to do ? \n A.Add\n B.Subtract\n C.Multiply\n D.Divide\n E.Square\n F.Power\n O.Exit program")
-        print("(Square operation work only for the first number input)")
-        i = str(input(" >>".lower()))
-        x = eval(input("Enter your First number:"))
-        y = eval(input("Enter your Second number:"))
-        # conditional
-        if i == "a":
-            print("The result is ", float(x + y))
-        elif i == "b":
-            print("The result is ", float(x - y))
-        elif i == "c":
-            print("The result is ", float(x * y))
-        elif i == "d":
-            print("The result is ", float(x / y))
-        elif i == "e":
-            print("The result is ", float(math.sqrt(x)))
-        elif i == "f":
-            print("The result is ", float(math.pow(x, y)))
-        elif i == "o":
-            break
-        #Invalid input
-        else:
-            print("Invalid input")
-            return
-        # continue or break the loop
-        j = str(input("Do you want to do another calculation ?\n>>"))
-        if j != "yes":
-            print("Ok, thanks")
-            break
+def add(num1, num2):
+    return num1 + num2
+def min(num1, num2):
+    return num1 - num2
+def multi(num1, num2):
+    return num1 * num2
+def divide(num1, num2):
+    return num1 / num2
+
+while True:
+    user_input = input("Enter the operation you want (+) (-) (:) (/) (q to quit): ")
+    num1 = eval(input("Num 1: "))
+    num2 = eval(input("Num 2: "))
+    if user_input == "+":
+        print(add(num1, num2))
+    elif user_input == "-":
+        print(min(num1, num2))
+    elif user_input == "*":
+        print(multi(num1, num2))
+    elif user_input == "/":
+        print(divide(num1, num2))
+    elif user_input == "q":
+        break
+        
+    else:
+        print("Invalid")
